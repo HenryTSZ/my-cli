d2f66216be1a107f27025c2ff6a40d32b74864fc
henry <lh69jh@gmail.com>
2020-09-18 14:00:02 +0800

    增加决策中心

--- constant/index.js
+++ constant/index.js
@@ -1,31 +1,56 @@
-exports.PATH_MAP = {
-  BridgeProduction: {
-    'bridge-business-web': 'source-web/BridgeProduction',
-    'source-web': 'BridgeProduction',
-    BridgeProduction: ''
+const list = [
+  {
+    name: 'BridgeProduction(桥梁)',
+    value: 'BridgeProduction',
+    path: {
+      'bridge-business-web': 'source-web/BridgeProduction',
+      'source-web': 'BridgeProduction',
+      BridgeProduction: ''
+    },
+    api: 'apiB',
+    ico: 'bridgeProduction'
   },
-  BaseProduction: {
-    'basis-facility-web': 'bimbasisstatic/BaseProduction',
-    bimbasisstatic: 'BaseProduction',
-    BaseProduction: ''
+  {
+    name: 'DecisionProduction(决策)',
+    value: 'DecisionProduction',
+    path: {
+      'bridge-business-web': 'source-web/DecisionProduction',
+      'source-web': 'DecisionProduction',
+      DecisionProduction: ''
+    },
+    api: 'apiB',
+    ico: 'decisionProduction'
   },
-  EnterpriseProduction: {
-    'basis-facility-web': 'bimbasisstatic/EnterpriseProduction',
-    bimbasisstatic: 'EnterpriseProduction',
-    EnterpriseProduction: ''
+  {
+    name: 'BaseProduction(基建项目)',
+    value: 'BaseProduction',
+    path: {
+      'basis-facility-web': 'bimbasisstatic/BaseProduction',
+      bimbasisstatic: 'BaseProduction',
+      BaseProduction: ''
+    },
+    api: 'apiP',
+    ico: 'baseProduction'
+  },
+  {
+    name: 'EnterpriseProduction(基建企业)',
+    value: 'EnterpriseProduction',
+    path: {
+      'basis-facility-web': 'bimbasisstatic/EnterpriseProduction',
+      bimbasisstatic: 'EnterpriseProduction',
+      EnterpriseProduction: ''
+    },
+    api: 'apiP',
+    ico: 'baseProduction'
   }
-}
+]
 
-exports.API_MAP = {
-  BridgeProduction: 'apiB',
-  BaseProduction: 'apiP',
-  EnterpriseProduction: 'apiP'
-}
+exports.PROJECT_LIST = list.map(({ name, value }) => ({ name, value }))
+
+exports.PATH_MAP = Object.fromEntries(list.map(({ value, path }) => ({ [value]: path })))
+
+exports.API_MAP = Object.fromEntries(list.map(({ value, api }) => ({ [value]: api })))
 
 exports.REPLACE_KEYWORDS = [/replaceProjectTitle/g, /replaceBaseApi/g, /replaceOutputDir/g]
 
-exports.ICO_NAME_MAP = {
-  BridgeProduction: 'bridgeProduction',
-  BaseProduction: 'baseProduction',
-  EnterpriseProduction: 'baseProduction'
-}
+exports.ICO_NAME_MAP = Object.fromEntries(list.map(({ value, ico }) => ({ [value]: ico })))
